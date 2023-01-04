3cea80270a73b3b87792fd893a9b5946b7e620a3
ikram <fullstackkdev@gmail.com>
2023-01-04 16:47:39 +0500

    latest without dialog

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitIncomingActivity.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitIncomingActivity.kt
@@ -305,25 +305,7 @@ class CallkitIncomingActivity : Activity() {
         sendBroadcast(intent)
         finish()
     }
-    private fun showDialog(title: String) {
-        val dialog = Dialog(activity)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(false)
-        dialog.setContentView(R.layout.custom_layout)
-        val body = dialog.findViewById(R.id.body) as TextView
-        body.text = title
-        val yesBtn = dialog.findViewById(R.id.yesBtn) as Button
-        val noBtn = dialog.findViewById(R.id.noBtn) as Button
-        yesBtn.setOnClickListener {
-            dialog.dismiss()
-        }
-        noBtn.setOnClickListener { 
-            dialog.dismiss() 
-        }
-        dialog.show()
     
-    }
-
     private fun getPicassoInstance(context: Context, headers: HashMap<String, Any?>): Picasso {
         val client = OkHttpClient.Builder()
                 .addNetworkInterceptor { chain ->
